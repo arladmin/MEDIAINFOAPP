7565ea485ee630783be7aed9cdb67049c4ce37b2
Dair Sanchez <dairsanchez@gmail.com>
2022-01-04 22:52:49 -0500

    express api created

---
+++ index.js
@@ -0,0 +1,44 @@
+const ffprobe = require("ffprobe");
+const ffprobeStatic = require("ffprobe-static");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(express.static(path.join(__dirname + "/uploads")));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSufix = Date.now() + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSufix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage });
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/mediainfo", upload.single("file"), (req, res) => {
+  ffprobe(req.file.path, { path: ffprobeStatic.path }, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(info);
+      res.json({ info });
+    }
+  });
+});
+app.listen(5000, () => {
+  console.log("App is listening on port 5000");
+});
